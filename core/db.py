76ee3ff447e614f848a6b44b748fbe0e18cc390e
triumph101LAB <odilitriumphifeatu@gmail.com>
2026-06-22 10:12:56 +0100

    Few modifications

--- core/db.py
+++ core/db.py
@@ -10,7 +10,10 @@ engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 )
-
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
